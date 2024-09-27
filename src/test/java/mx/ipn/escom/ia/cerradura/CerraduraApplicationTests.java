b9c09b6a51aab631f38b26cc5d3144e5aa40be74
janmayos <jangasca5@gmail.com>
2024-09-27 12:18:31 -0600

    Init Spring e index.html

---
+++ src/test/java/mx/ipn/escom/ia/cerradura/CerraduraApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.ipn.escom.ia.cerradura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerraduraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
