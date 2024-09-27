b5e0d5cd0e280e1c527c6fd27454f64c2c4444a3
janmayos <jangasca5@gmail.com>
2024-09-27 13:06:08 -0600

    Se implementa el controller de Cerradura
    Se utiliza el mapping /cerradura/estrella/numero y
    /cerradura/positiva/numero
    Para ejecutar el servicio de cerradura y regresarlo al cliente

--- src/main/java/mx/ipn/escom/ia/cerradura/controller/Index.java
+++ src/main/java/mx/ipn/escom/ia/cerradura/controller/Index.java
@@ -2,6 +2,7 @@ package mx.ipn.escom.ia.cerradura.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 
 public class Index {
