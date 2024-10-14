9a500cbcc190c565927696d3c6752c5337d6ee09
janmayos <jangasca5@gmail.com>
2024-10-13 20:58:32 -0600

    Login

--- src/main/java/mx/ipn/escom/ia/cerradura/controller/Index.java
+++ src/main/java/mx/ipn/escom/ia/cerradura/controller/Index.java
@@ -2,14 +2,17 @@ package mx.ipn.escom.ia.cerradura.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 
 public class Index {
 
 	@RequestMapping("/")
-	String home() {
-		return "Hola mundo con controller";
+	public ModelAndView  home() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login.html");
+		return modelAndView;
 	}
 
 }
