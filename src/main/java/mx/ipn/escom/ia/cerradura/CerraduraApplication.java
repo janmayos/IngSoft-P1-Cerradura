51732c68838b93ec93c8d4e92ebb9eec398c3556
Jan Mario Gasca Molinero <jangasca5@gmail.com>
2025-01-08 19:06:42 -0600

    .env agregado

--- src/main/java/mx/ipn/escom/ia/cerradura/CerraduraApplication.java
+++ src/main/java/mx/ipn/escom/ia/cerradura/CerraduraApplication.java
@@ -2,15 +2,14 @@ package mx.ipn.escom.ia.cerradura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class CerraduraApplication {
 
-
-	public static void main(String[] args) {
-		SpringApplication.run(CerraduraApplication.class, args);
-		
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        SpringApplication.run(CerraduraApplication.class, args);
+    }
 
 }
